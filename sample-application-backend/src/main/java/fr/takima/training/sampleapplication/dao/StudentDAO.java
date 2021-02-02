3f3487bbc13aeb9471471e3a51de3eca5d2553c8
Corentinver <vergnon.corentin@gmail.com>
2021-02-02 17:34:13 +0100

    c

--- sample-application-backend/src/main/java/fr/takima/training/sampleapplication/dao/StudentDAO.java
+++ sample-application-backend/src/main/java/fr/takima/training/sampleapplication/dao/StudentDAO.java
@@ -11,4 +11,5 @@ public interface StudentDAO extends JpaRepository<Student, Long> {
     List<Student> findStudentsByDepartment_Name(String departmentName);
     int countAllByDepartment_Name(String departmentName);
     Student getById(long id);
+	// test
 }
